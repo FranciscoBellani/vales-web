c13ba0ff99cfa60f9ecb2e1ac2f18303b2e79641
FranciscoBellani <FranciscoBellani@users.noreply.github.com>
2024-07-05 01:47:25 -0300

    Modificaciones en inicio.js y app.js

---
+++ src/Paginas/Inicio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inicio = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la Carta de Menú con Videos</h1>
+      <p>Esta es la página de inicio de la aplicación. Aquí podrías incluir una breve descripción de tu aplicación.</p>
+    </div>
+  );
+}
+
+export default Inicio;
---
+++ src/Paginas/ListadoPlatos.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListadoPlatos = () => {
+  return (
+    <div>
+      <h1>Listado de Platos</h1>
+      {/* Aquí iría la lógica para mostrar los platos según la categoría */}
+    </div>
+  );
+}
+
+export default ListadoPlatos;
