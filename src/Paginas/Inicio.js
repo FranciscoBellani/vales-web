258aac67dbd80803939f0d3e283d0f1c5f3e09d7
FranciscoBellani <FranciscoBellani@users.noreply.github.com>
2024-07-24 21:48:00 -0300

    Embebimos video de youtube acomodamos estilo

--- src/Paginas/Inicio.js
+++ src/Paginas/Inicio.js
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import '../Estilos/styles.css'; // Asegúrate de que esta ruta sea correcta
+import '../Estilos/styles.css';
 
 const categorias = [
-  { titulo: 'Entradas', descripcion: 'Deliciosas entradas', video: 'link-a-video' },
-  { titulo: 'Platos principales', descripcion: 'Sabrosos platos principales', video: 'link-a-video' },
-  { titulo: 'Postres', descripcion: 'Irresistibles postres', video: 'link-a-video' },
-  { titulo: 'Bebidas', descripcion: 'Refrescantes bebidas', video: 'link-a-video' },
+  { titulo: 'Entradas', descripcion: 'Deliciosas entradas', video: 'https://www.youtube.com/embed/esIyz899wlE?autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&rel=0' },
+  { titulo: 'Platos principales', descripcion: 'Sabrosos platos principales', video: 'https://www.youtube.com/embed/sqelF9LaAlQ?autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&rel=0'},
+  { titulo: 'Postres', descripcion: 'Irresistibles postres', video: 'https://www.youtube.com/embed/VIDEO_ID_3' },
+  { titulo: 'Bebidas', descripcion: 'Refrescantes bebidas', video: 'https://www.youtube.com/embed/VIDEO_ID_4' },
 ];
 
 const Inicio = () => {
@@ -22,7 +22,16 @@ const Inicio = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     vertical: true,
-    verticalSwiping: true
+    verticalSwiping: true,
+    appendDots: dots => (
+      <div className="carousel-indicators-vertical">
+        <ul> {dots} </ul>
+      </div>
+    ),
+    customPaging: i => (
+      <div className="custom-dot"></div>
+    ),
+    arrows: false // Deshabilitamos los botones Next y Prev
   };
 
   const handleCategoriaClick = (categoria) => {
@@ -31,16 +40,22 @@ const Inicio = () => {
 
   return (
     <div className="inicio">
-      <div className="container mt-5">
+      <div className="slider-container">
         <Slider {...settings}>
           {categorias.map((categoria, index) => (
             <div key={index} className="categoria" onClick={() => handleCategoriaClick(categoria)}>
               <h2>{categoria.titulo}</h2>
               <p>{categoria.descripcion}</p>
-              <video controls>
-                <source src={categoria.video} type="video/mp4" />
-                Tu navegador no soporta la etiqueta de video.
-              </video>
+              <div className="video-container">
+                <iframe
+                 src={categoria.video}
+                 title={categoria.titulo}
+                 frameBorder="0"
+                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                 allowFullScreen
+                 style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                ></iframe>
+              </div>
             </div>
           ))}
         </Slider>
